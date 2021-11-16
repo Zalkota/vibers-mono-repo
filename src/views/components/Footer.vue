21399ff0d7c9d59b843aeaa06936a827d85bb90f
zalkota <dominicmazzola@gmail.com>
2021-11-16 09:28:09 -0500

    Video.css changes, home changes, added images.

--- src/views/components/Footer.vue
+++ src/views/components/Footer.vue
@@ -2,7 +2,7 @@
   <div class="section  text-white h-full bg-gray-300 " style=" min-height: 100px;">
       <div class="w-full text-center relative">
 
-          <img src="@/assets/images/space_background_4k.jpg" class="invisible md:visible" style="margin:0 auto; width:100%;  " />
+          <img src="@/assets/images/cosmic_factory.jpg" class="invisible md:visible" style="margin:0 auto; width:100%;  " />
 
       </div>
 
